42753f2100e0e6c6eaecf6b6b8b56086cf6ecf1b
sam-tahiri <tahiri.smao@gmail.com>
2024-02-28 20:48:42 +0100

    sam-tahiri
    add some effects on css

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+// navigation bar effects on scrol
+window.addEventListener("scroll", function(){
+  const header_position = document.querySelector(".header-position");
+  header_position.classList.toggle("sticky", window.scrollY > 0)
+});
+
+// display menu phone
 let menuphone = document.querySelector(".menu-phone");
 let navphone = document.querySelector(".nav-phone");
 
@@ -5,6 +12,7 @@ menuphone.addEventListener("click", () => {
     navphone.classList.toggle("active");
 });
 
+// configration slider of testimonainl
 var swiper = new Swiper(".custem", {
     effect: "coverflow",
     grabCursor: true,
